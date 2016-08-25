606dcf225600e6b74d7be7a2f706ad907da64143
Ubuntu <ubuntu@ip-172-31-9-14.us-west-2.compute.internal>
2016-08-25 05:50:52 +0000

    Hello World

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+echo "Hello World";
+
+?>
+
+/**
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
 /**
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
@@ -13,6 +22,7 @@
  * @since         0.10.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
+/**
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
 
+*/
